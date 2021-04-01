6c474eed2714cd900a093f85bd6d856fb5724f93
drw2005 <33473981+drw2005@users.noreply.github.com>
2021-04-01 21:40:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,9 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# 获取hello world和依赖
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/helloworld/luci-app-vssr
+./scripts/feeds update -a
+./scripts/feeds install -a
