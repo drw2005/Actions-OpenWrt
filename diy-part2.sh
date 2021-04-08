2329aac8fe567d38b7cd7bbae4527851b8e11632
drw2005 <33473981+drw2005@users.noreply.github.com>
2021-04-08 08:18:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,7 +25,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 设置密码（修改自己想要的密码）
-sed -i 's/root::0:0:99999:7:::/root:$1$bHXZ9kNl$MA79kAoAnzIRdaW9.FDT0/:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$sx/QMBQO$j9ew137mSBSHLyU5lY2L./:18723:0:99999:7:::/g' /etc/shadow
 
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
