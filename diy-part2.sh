fadd8441decbd3e6aa48b84ac03a89a6643b9bf8
drw2005 <33473981+drw2005@users.noreply.github.com>
2021-04-09 11:42:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,8 +25,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 设置密码（修改自己想要的密码）
-sed -i 's/root::0:0:99999:7:::/root:$1$eQEUOItJ$AcwLFdf8hoJMt6x3zBRJk1:18726:0:99999:7:::/g' /etc/shadow
-# sed -i 's/root:1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$eQEUOItJ$AcwLFdf8hoJMt6x3zBRJk1:18726:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/root::0:0:99999:7:::/root:$1$eQEUOItJ$AcwLFdf8hoJMt6x3zBRJk1:18726:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$eQEUOItJ$AcwLFdf8hoJMt6x3zBRJk1:18726:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
